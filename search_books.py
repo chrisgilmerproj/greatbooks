c73823b3436fdd38d5f80d04a33ae0d62b23307c
Chris Gilmer <chris.gilmer@gmail.com>
2012-03-28 17:15:02 -0700

    Added a bunch of useful terms to search for

--- search_books.py
+++ search_books.py
@@ -1,16 +1,39 @@
 #! /usr/local/bin/python
 
+import random
 import sys
 
 import pyes
 
 import settings
 
+SYNTOPICON_01 = ['Angel', 'Animal', 'Aristocracy', 'Art', 'Astronomy',
+    'Beauty', 'Being', 'Cause', 'Chance', 'Change', 'Citizen', 'Constitution',
+    'Courage', 'Custom and Convention', 'Definition', 'Democracy', 'Desire',
+    'Dialectic', 'Duty', 'Education', 'Element', 'Emotion', 'Eternity',
+    'Evolution', 'Experience', 'Family', 'Fate', 'Form', 'God',
+    'Good and Evil', 'Government', 'Habit', 'Happiness', 'History', 'Honor',
+    'Hypothesis', 'Idea', 'Immortality', 'Induction', 'Infinity', 'Judgment',
+    'Justice', 'Knowledge', 'Labor', 'Language', 'Law', 'Liberty',
+    'Life and Death', 'Logic', 'and Love']
+SYNTOPICON_02 = ['Man', 'Mathematics', 'Matter', 'Mechanics', 'Medicine',
+    'Memory and Imagination', 'Metaphysics', 'Mind', 'Monarchy', 'Nature',
+    'Necessity and Contingency', 'Oligarchy', 'One and Many', 'Opinion',
+    'Opposition', 'Philosophy', 'Physics', 'Pleasure and Pain', 'Poetry',
+    'Principle', 'Progress', 'Prophecy', 'Prudence', 'Punishment', 'Quality',
+    'Quantity', 'Reasoning', 'Relation', 'Religion', 'Revolution', 'Rhetoric',
+    'Same and Other', 'Science', 'Sense', 'Sign and Symbol', 'Sin', 'Slavery',
+    'Soul', 'Space', 'State', 'Temperance', 'Theology', 'Time', 'Truth',
+    'Tyranny', 'Universal and Particular', 'Virtue and Vice', 'War and Peace',
+    'Wealth', 'Will', 'Wisdom', 'and World']
+TERM_LIST = SYNTOPICON_01 + SYNTOPICON_02
+
 
 def main(search_terms, max_results):
     """
     This searches books
     """
+    print 'Searching documents for %s results of: %s' % (max_results, search_terms)
 
     conn = pyes.ES(settings.ES_CLUSTER)
 
@@ -28,7 +51,7 @@ def main(search_terms, max_results):
 
 
 if __name__ == "__main__":
-    terms = ['something']
+    terms = [random.choice(TERM_LIST).lower()]
     max_results = 5
     if len(sys.argv) >= 2:
         terms = sys.argv[1].split()
